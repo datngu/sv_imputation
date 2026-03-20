e791e02025b31160f3546516d6f7849e658651ef
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2026-03-20 20:09:44 +0100

    fix data path HPC

--- 2b.run_saga_2stages.sh
+++ 2b.run_saga_2stages.sh
@@ -22,8 +22,8 @@ nextflow run main.nf \
     --genome_build_37 data/genomes/GRCh37.fa \
     --genome_build_38 data/genomes/GRCh38.fa \
     --chain_file data/genomes/GRCh37_to_GRCh38.chain.gz \
-    --impute_ref_panel data/ref_panel/panel.888samples.full.vcf.gz \
-    --snp_ref_panel_dir /path/to/HC_1000G_hg38/rsid_vcfs \
+    --impute_ref_panel /cluster/projects/nn9114k/datngu/database/elife_2025_sv_panel/panel.888samples.full.vcf.gz \
+    --snp_ref_panel_dir /cluster/projects/nn9114k/datngu/database/HC_1000G_hg38/rsid_vcfs \
     --plink_map_dir data/plink.GRCh38.map/no_chr_in_chrom_field \
     --imputation_mode 2stage \
     --outdir imputation_output_2stage \
