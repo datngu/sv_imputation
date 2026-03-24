3cab3803fc80cb27b9a44a7b0341fea6010d29e8
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2026-03-24 19:24:37 +0100

    add run 1 stage

--- 2c.run_saga_1stage.sh
+++ 2c.run_saga_1stage.sh
@@ -16,14 +16,14 @@ export SINGULARITY_CACHEDIR="/cluster/projects/nn9114k/datngu/singularity"
 export SINGULARITY_TMPDIR="/cluster/projects/nn9114k/datngu/singularity/tmp"
 export NXF_SINGULARITY_CACHEDIR="/cluster/projects/nn9114k/datngu/singularity"
 
-nextflow run main_1_stage.nf \\
-    -profile saga,singularity,sv_impute \\
-    --input_manifest data/input_manifest.csv \\
-    --genome_build_37 data/genomes/GRCh37.fa \\
-    --genome_build_38 data/genomes/GRCh38.fa \\
-    --chain_file data/genomes/GRCh37_to_GRCh38.chain.gz \\
-    --sv_ref_panel /cluster/projects/nn9114k/datngu/database/elife_2025_sv_panel/panel.888samples.full.vcf.gz \\
-    --plink_map_dir data/plink.GRCh38.map/no_chr_in_chrom_field \\
-    --outdir imputation_output_1stage \\
-    --trace_dir imputation_traces_1stage \\
+nextflow run main_1_stage.nf \
+    -profile saga,singularity,sv_impute \
+    --input_manifest data/input_manifest.csv \
+    --genome_build_37 data/genomes/GRCh37.fa \
+    --genome_build_38 data/genomes/GRCh38.fa \
+    --chain_file data/genomes/GRCh37_to_GRCh38.chain.gz \
+    --sv_ref_panel /cluster/projects/nn9114k/datngu/database/elife_2025_sv_panel/panel.888samples.full.vcf.gz \
+    --plink_map_dir data/plink.GRCh38.map/no_chr_in_chrom_field \
+    --outdir imputation_output_1stage \
+    --trace_dir imputation_traces_1stage \
     -resume
